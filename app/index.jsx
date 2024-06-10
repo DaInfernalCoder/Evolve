e0ddb8d23770340af3bd287a24a9a2946c82a5bd
DaInfernalCoder <111019954+DaInfernalCoder@users.noreply.github.com>
2024-06-10 14:31:48 -0500

    App backbone

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue">
+      <Text className = "text-5xl" >
+        Hello World!
+      </Text>
+      <StatusBar style="auto" />
+      <Link href = "/profile" style = {{color: "blue"}}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
